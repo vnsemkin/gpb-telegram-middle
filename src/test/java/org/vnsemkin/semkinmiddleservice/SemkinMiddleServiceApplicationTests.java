d9749d9c1c2e98221d5d36b057df6d7aad6a6e3d
vnsemkin <vnsemkin@gmail.com>
2024-05-18 13:24:59 +0300

    Initial commit

---
+++ src/test/java/org/vnsemkin/semkinmiddleservice/SemkinMiddleServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package org.vnsemkin.semkinmiddleservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SemkinMiddleServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
