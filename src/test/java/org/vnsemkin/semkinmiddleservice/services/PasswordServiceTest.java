3115d1a5519431ee57dd5e5318dbff28c7a11c97
vnsemkin <vnsemkin@gmail.com>
2024-06-03 15:41:38 +0300

    Задание 6. Изменен PasswordServiceTest

--- src/test/java/org/vnsemkin/semkinmiddleservice/services/PasswordServiceTest.java
+++ src/test/java/org/vnsemkin/semkinmiddleservice/services/PasswordServiceTest.java
@@ -1,7 +1,12 @@
 package org.vnsemkin.semkinmiddleservice.services;
 
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.vnsemkin.semkinmiddleservice.SemkinMiddleServiceApplicationTests;
 import org.vnsemkin.semkinmiddleservice.domain.services.PasswordService;
 
@@ -9,7 +14,9 @@ import static org.junit.jupiter.api.Assertions.assertNotEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
-public class PasswordServiceTest extends SemkinMiddleServiceApplicationTests {
+
+@ExtendWith(MockitoExtension.class)
+public class PasswordServiceTest {
     final static String PASSWORD = "password";
     final static String HASHED_PASSWORD = "$2a$10$/7rukcWx053I1ESyqShGJugQ28wjc2JukJTpEMR4tQo9AW2T.DI4W";
 
